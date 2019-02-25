cf47740939d9f3ab177e70438f218b12b2c9f9e4
Owen <Owen@0102L>
2019-02-25 15:04:00 -0700

    Initial commit. Commented methods

---
+++ CSE360Assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,67 @@
+package cse360assign2;
+
+/** Class ID: 518
+ * @author Owen O'Toole
+ * @version Feb 23, 2019
+ * 
+ * @category Assignment 2: Version 1 - Documented Code
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Constructor that initializes the Calculator class with the total calculations set to 0.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * getTotal method returns current total of calculations
+	 * @return the current result of all calculations
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add method accepts integer value and adds it to total
+	 * @param value - the integer added to the current total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract method accepts an integer value and subtracts it from total
+	 * @param value - the integer subtracted from current total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply method accepts an integer value and multiplies the total
+	 * @param value - the integer to multiply the current total by
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide method accepts an integer value and divides the total
+	 * @param value - the integer to divide the current total by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * getHistory method returns a string of the history of calculations done
+	 * @return - the string of calculations done up to current point
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
