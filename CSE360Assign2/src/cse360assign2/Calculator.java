4a71bc7b1c29f754f8ad47fa819ee7e55429e0e8
Owen <ootoole@asu.edu>
2019-02-25 19:26:07 -0700

    Version 3 Updates:
    getTotal now correctly returns total, not 0.
    getHistory method now implemented.
    history string is initialized in the constructor and is now tracked
    through each math function method.
    Better class description for Calculator.java added.

--- CSE360Assign2/src/cse360assign2/Calculator.java
+++ CSE360Assign2/src/cse360assign2/Calculator.java
@@ -2,19 +2,23 @@ package cse360assign2;
 
 /** Class ID: 518
  * @author Owen O'Toole
- * @version Feb 23, 2019
+ * @version Feb 25, 2019
  * 
- * @category Assignment 2: Version 2 - Basic Calculator functions implemented
+ * @category Assignment 2: Version 3 - Basic Calculator functions and function history implemented.
+ * Calculator.java contains the calculator class. This class has methods for the 4 basic mathematical functions: addition, subtraction,
+ * multiplication and division. The history method allows a complete history of all calculations performed to be accessed. The getTotal
+ * method returns the current total.
  */
 public class Calculator {
 
 	private int total;
-	
+	private String history;
 	/**
-	 * Constructor that initializes the Calculator class with the total calculations set to 0.
+	 * Constructor that initializes the Calculator class with the total calculations and history set to 0
 	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	/**
@@ -22,7 +26,7 @@ public class Calculator {
 	 * @return the current result of all calculations
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
@@ -30,6 +34,7 @@ public class Calculator {
 	 * @param value - the integer added to the current total
 	 */
 	public void add (int value) {
+		history = history + " + " + value; //Adds value addition to history
 		total = total + value; //Adds value to total
 	}
 	
@@ -38,6 +43,7 @@ public class Calculator {
 	 * @param value - the integer subtracted from current total
 	 */
 	public void subtract (int value) {
+		history = history + " - " + value; //Adds value subtraction to history
 		total = total - value; //Subtracts value from total
 	}
 	
@@ -46,6 +52,7 @@ public class Calculator {
 	 * @param value - the integer to multiply the current total by
 	 */
 	public void multiply (int value) {
+		history = history + " * " + value; //Adds value multiplication to history
 		total = total * value; //Multiplies total by value
 	}
 	
@@ -54,6 +61,7 @@ public class Calculator {
 	 * @param value - the integer to divide the current total by
 	 */
 	public void divide (int value) {
+		history = history + " / " + value; //Adds value division to history
 		if (value == 0) //Checks to see if division by 0 occurs
 		{
 			total = 0; //Total will be set to 0 if the denominator is 0
@@ -69,6 +77,6 @@ public class Calculator {
 	 * @return - the string of calculations done up to current point
 	 */
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
